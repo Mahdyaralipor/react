ad4cbfa40108d8931a18d6138914cf02e4b04881
mahdyaralipor <mahdyaralipor0@gmail.com>
2025-09-28 07:35:16 +0330

    Use Variable

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,7 +1,8 @@
 export function Header () {
+    let Title = "Header"
     return (
         <>
-            <h1>Header</h1>
+            <h1>{ Title }</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni enim, optio ut eum velit non obcaecati laudantium aut exercitationem blanditiis accusantium minus excepturi quos aperiam corporis suscipit necessitatibus dicta expedita.</p>
         </>
     )
