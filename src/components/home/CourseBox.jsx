dbf0d0fcec77c671e4d855bf12cb35aa1020e256
mahdyaralipor <mahdyaralipor0@gmail.com>
2025-09-28 08:14:23 +0330

    What Props

---
+++ src/components/home/CourseBox.jsx
@@ -0,0 +1,9 @@
+export function CourseBox() {
+    return(
+        <div>
+            <h2>Course Title</h2>
+            <p>Course Ditails</p>
+            <span>time: 10</span>
+        </div>
+    )
+}
--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,8 +1,14 @@
+import { CourseBox } from "./CourseBox"
+
 export function Main () {
     return (
         <>
             <h1>Main</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rerum illo, cumque inventore accusamus odit dicta esse vero ea suscipit facilis qui, quas, maxime quod voluptatum necessitatibus unde voluptatibus. Qui.</p>
+            <CourseBox title="Course One"/>
+            <CourseBox title="Vue Course"/>
+            <CourseBox title="React Course"/>
+
         </>
     )
 }
