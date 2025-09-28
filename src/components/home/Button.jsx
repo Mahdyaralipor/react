aad53c759835e5a862337c6dff843fa7fba1721a
mahdyaralipor <mahdyaralipor0@gmail.com>
2025-09-28 10:12:07 +0330

    event manager

---
+++ src/components/home/Button.jsx
@@ -0,0 +1,9 @@
+export function Button() {
+
+    const Click = () => {
+        console.log("clickkkkkkkkkkkkkk")
+    }
+    return (
+        <button onClick={Click}>Click Me!</button>
+    )
+}
--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -1,5 +1,5 @@
-import { CourseBox } from "./CourseBox"
-import { CourseList } from "./CourseList"
+import { Button } from "./Button"
+
 
 export function Main () {
     // let courseOne =  {
@@ -11,15 +11,7 @@ export function Main () {
         <>
             <h1>Main</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat rerum illo, cumque inventore accusamus odit dicta esse vero ea suscipit facilis qui, quas, maxime quod voluptatum necessitatibus unde voluptatibus. Qui.</p>
-            {/* <CourseBox course={courseOne}/> */}
-            <CourseList>
-                <CourseBox title="React ..." />
-                <CourseBox title="Vue ..." />
-
-            </CourseList>
-            <CourseList>
-                <CourseBox title="Laravel ..." />
-            </CourseList>
+            <Button />
             
         </>
     )
